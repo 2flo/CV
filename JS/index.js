e48725c549b5da5bfea4c8bc8acac174c0ed8831
2flo <dianeflozin@gmail.com>
2021-03-07 17:10:04 +0100

    Changement d'image quand on active le bouton menu

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,10 @@
 const containerMenu = document.querySelector('.container-menu');
 const btnMenu = document.querySelector('.btn-menu');
+const line = document.getElementsByClassName('.line');
 
 //On crée une fonction pour activer ou désactiver un événement lorsqu'on clique sur le bouton menu
 
 btnMenu.addEventListener('click', () => {
     containerMenu.classList.toggle('active')
+    line.src="<i class='fas fa-times'></i>";
 })
