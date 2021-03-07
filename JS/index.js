91d57db333806d9b5e00ef8c2931d0cf14614cd0
2flo <dianeflozin@gmail.com>
2021-03-07 17:29:43 +0100

    Tentative de modification pour le changement d'image

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,8 @@
 const containerMenu = document.querySelector('.container-menu');
 const btnMenu = document.querySelector('.btn-menu');
-const line = document.getElementsByClassName('.line');
 
 //On crée une fonction pour activer ou désactiver un événement lorsqu'on clique sur le bouton menu
 
 btnMenu.addEventListener('click', () => {
-    containerMenu.classList.toggle('active')
-    line.src="<i class='fas fa-times'></i>";
+    containerMenu.classList.toggle('active').src="<i class='fas fa-times'></i>";
 })
