198cecb7b2dd49988e257e3e193e77c65709d3ee
2flo <dianeflozin@gmail.com>
2021-03-07 17:46:41 +0100

    Nouvelle tentative dans le changement d'image

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,10 @@
 const containerMenu = document.querySelector('.container-menu');
 const btnMenu = document.querySelector('.btn-menu');
+const faBars = document.getElementsByClassName('.fa-bars');
 
 //On crée une fonction pour activer ou désactiver un événement lorsqu'on clique sur le bouton menu
 
 btnMenu.addEventListener('click', () => {
-    containerMenu.classList.toggle('active').src="<i class='fas fa-times'></i>";
+    containerMenu.classList.toggle('active');
+    faBars.src = '<i class="fas fa-times"></i>';
 })
